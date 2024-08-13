b076b7e7fb834ec4f3c245c26a3e7de9922910f1
distribucion <95579248+distribucion@users.noreply.github.com>
2024-08-13 23:02:52 +0200

    allow cors

--- main.py
+++ main.py
@@ -8,40 +8,40 @@ from fastapi.responses import PlainTextResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
-
-# routers: comment out next line till create them
+# routers: comentar hasta que los crees
 from routers import todos
 
 import config
 
 app = FastAPI()
 
-# router: comment out next line till create it
+# routers: comentar hasta que los crees
 app.include_router(todos.router)
 
-
+# Lista de orígenes permitidos para CORS
 origins = [
     "http://localhost:3000",
-    "https://frontend-netxjs-versel-dm-jnfn.vercel.app/",
+    "https://frontend-netxjs-versel-dm-jnfn.vercel.app",
 ]
 
-# CORS configuration, needed for frontend development
+# Configuración de CORS para permitir solicitudes desde los orígenes específicos
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,  # Usar la lista de orígenes definidos
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# Manejador global de excepciones HTTP
 
-# global http exception handler, to handle errors
+
 @app.exception_handler(StarletteHTTPException)
 async def http_exception_handler(request, exc):
     print(f"{repr(exc)}")
     return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
 
-# to use the settings
+# Uso de configuración de la aplicación con lru_cache
 
 
 @lru_cache()
@@ -51,7 +51,7 @@ def get_settings():
 
 @app.get("/")
 def read_root(settings: config.Settings = Depends(get_settings)):
-    # print the app_name configuration
+    # Imprimir la configuración app_name
     print(settings.app_name)
     return "Hello World"
 
